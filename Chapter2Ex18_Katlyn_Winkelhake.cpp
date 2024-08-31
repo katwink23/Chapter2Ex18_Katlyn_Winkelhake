58f705f040e844f6b36554cf3e67650eabfae97b
Katlyn W. <krw8ateez@gmail.com>
2024-08-31 11:33:14 -0400

    Add project files.

---
+++ Chapter2Ex18_Katlyn_Winkelhake.cpp
@@ -0,0 +1,19 @@
+/*
+Title: Chapter 2 Exercise 18
+Programmer: Katlyn Winkelhake
+File Name: Chapter2Ex18_Katlyn_Winkelhake
+Date: Aug 21 2024
+Requirements:
+A soft drink company recently surveyed 16,500 of its customers and found that approximately 15 percent of those surveyed purchase one or more energy drinks per week. Of those customers who purchase energy drinks, approximately 58 percent of them prefer citrus-flavored energy drinks. Write a program that displays the following:
+
+• The approximate number of customers in the survey who purchase one or more energy drinks per week.
+
+• The approximate number of customers in the survey who prefer citrus-flavored energy drinks.
+
+Once you have coded this and placed in your main branch in GitHub, create a second branch that revises your code so
+
+that the user can enter the number of the number of customers that were surveyed
+*/
+
+#include <iostream>
+
